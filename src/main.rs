4bdaa64fc71b732d0fd3298cfe7fed66f1394082
filmar <filippo.ufficiale@gmail.com>
2026-03-18 13:06:26 +0100

    return to zero

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,30 @@
+use std::task::Context;
+
+
+
 fn main() {
+    println!("dio saltinbanco");
 }
 
 
 struct Context;
 
 trait PlenaCode {
-    fn update_context(&self, ctx: Context) -> Context;
+    fn exec(&self, ctx: Context) -> Context;
 }
 
-enum SpellTest {
-    Calore, 
-    Energia,
-    Aumentare,
-    Diminuire,
-    Spingere,
-    Tirare,
-    Vita,
-    Morte,
-    Vicino,
-    Lontano,
-    Direzione,
-    Area,
-    Bersaglio,
-    Ora,
-    Dopo
+enum TestCode {
+    Caldo,
+    Incremento,
+    Decremento,
 }
 
-impl PlenaCode for SpellTest {
-    fn update_context(&self, ctx: Context) -> Context {
-        
+impl PlenaCode for TestCode {
+    fn exec(&self, ctx: Context) -> Context {
+        match self {
+            TestCode::Caldo => Context,
+            TestCode::Decremento =>  Context,
+            TestCode::Incremento =>  Context
+        }
     }
 }
